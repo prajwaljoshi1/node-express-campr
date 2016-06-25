df9931588edecc957357427a9ada609c7b92a66a
Prajwal Joshi <prazwal@outlook.com>
2016-06-26 04:13:01 +1000

    add basic comment system

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require('mongoose');
 var Campground = require('./models/campground.js');
+var Comment = require('./models/comment.js');
+
 var seedDB = require('./seeds');
 
 
@@ -35,7 +37,7 @@ app.get("/campgrounds", function(req, res){
 		if(err){
 			 console.log(err);
 		}else{
-			res.render("index", {campgrounds:allCampgrounds});
+			res.render("campgrounds/index", {campgrounds:allCampgrounds});
 		}
 	});
 });
@@ -43,7 +45,7 @@ app.get("/campgrounds", function(req, res){
 
 
 app.get("/campgrounds/new", function(req, res){
-	res.render("new.ejs");
+	res.render("campgrounds/new.ejs");
 });
 
 
@@ -83,12 +85,51 @@ app.get("/campgrounds/:id", function(req, res){
 			console.log("something went wrong");
 		}else{
 			console.log("=======================> ", foundCampground);
-			res.render("show", {campground: foundCampground});
+			res.render("campgrounds/show", {campground: foundCampground});
 		}
  });
+});
+
+// comments routes
+
+app.get('/campgrounds/:id/comments/new', function(req, res){
+	Campground.findById(req.params.id, function(err, campground){
+		if(err){
+			console.log(err);
+		}else{
+			console.log("====================> ",campground);
+				res.render("comments/new", {campground: campground});
+		}
+	})
+
+});
+
+
+app.post('/campgrounds/:id/comments', function(req, res){
+	//look up campground using id
+	console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+	Campground.findById(req.params.id, function(err, campground){
+			if(err){
+				console.log(err);
+				res.redirect("/campgrounds");
+			}else{
+					Comment.create(req.body.comment, function(err, comment){
+						if(err){
+							console.log(err);
+						}else{
+							campground.comments.push(comment);
+							campground.save();
+							res.redirect('/campgrounds/'+ campground._id)
+						}
+					});
+			}
+	});
+	// create new comments
+	//connect new comments to campground
+	//redirect
 });
 
 
 app.listen(3000, function(){
 	console.log("App running on 3000, baby!");
-})
+});
